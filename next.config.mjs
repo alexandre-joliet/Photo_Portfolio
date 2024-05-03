bcf56bac441bc188f6d966f608d6c9830c96f0b0
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-03 15:39:52 +0200

    :wrench: Add images path configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const account = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: `/${account}/**`,
+      },
+    ],
+  },
+};
 
 export default nextConfig;
