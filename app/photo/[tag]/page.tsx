4a3ed9b149e1545cc552badf553734fec826f277
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-15 16:35:07 +0200

    :truck: Rename component + structure improvment

--- app/photo/[tag]/page.tsx
+++ app/photo/[tag]/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/app/Components/Header/page";
 import styles from "../../page.module.css";
 import handleFetch from "@/utils/fetch";
 
-const Folder = async ({ params }: any) => {
+const Tag = async ({ params }: any) => {
   const tag = params.tag;
 
   const data = await handleFetch.fetchAllImages(tag);
@@ -12,26 +12,28 @@ const Folder = async ({ params }: any) => {
     <>
       <main className={styles.main}>
         <Header />
-        <h1 className={styles.page_title}>
-          <br />
-          {data.folderName}
-        </h1>
-        <div className={styles.images_container}>
-          {data.imagesArray.map((item: any) => (
-            <div key={item.asset_id} className={styles.image_wrapper}>
-              <Image
-                src={item.url}
-                fill
-                alt="Picture of the author"
-                className={styles.image}
-                sizes="(max-width: 1440px) 100vw, 50vw"
-              ></Image>
-            </div>
-          ))}
+        <div className={styles.main_wrapper}>
+          <h3 className={styles.page_title}>
+            <br />
+            {data.folderName}
+          </h3>
+          <div className={styles.images_container}>
+            {data.imagesArray.map((item: any) => (
+              <div key={item.asset_id} className={styles.image_wrapper}>
+                <Image
+                  src={item.url}
+                  fill
+                  alt="Picture of the author"
+                  className={styles.image}
+                  sizes="(max-width: 1440px) 100vw, 50vw"
+                ></Image>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </>
   );
 };
 
-export default Folder;
+export default Tag;
