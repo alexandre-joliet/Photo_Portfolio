be3cde47275da8f1a61e9e337dbc059e05b0cf6e
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-21 17:16:06 +0200

    :sparkles: Create new Component for modal

---
+++ app/Components/Modal/page.tsx
@@ -0,0 +1,56 @@
+import { ImageProps } from "@/utils/types";
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+
+type ModalProps = {
+  openModal: boolean;
+  closeModal: () => void;
+  images: ImageProps[];
+  selectedImage: ImageProps | any;
+};
+
+const Modal = ({
+  openModal,
+  closeModal,
+  images,
+  selectedImage,
+}: ModalProps) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    console.log(openModal);
+    // if (openModal) {
+    //   ref.current?.showModal();
+    // } else {
+    //   ref.current?.close();
+    // }
+  }, [openModal]);
+
+  console.log(selectedImage);
+
+  return (
+    <>
+      {openModal && (
+        <dialog open className={styles.modal_bg}>
+          <div className={styles.modal_image_container}>
+            <Image
+              src={selectedImage.url}
+              fill
+              alt="Picture of the author"
+              className={styles.modal_image}
+              sizes="(max-width: 1440px) 100vw, 80vw"
+            ></Image>
+          </div>
+          <form method="dialog" className={styles.modal_container}>
+            <button onClick={closeModal} className={styles.modal_button}>
+              X
+            </button>
+          </form>
+        </dialog>
+      )}
+    </>
+  );
+};
+
+export default Modal;
