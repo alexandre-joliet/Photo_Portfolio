c83ac08c79fd1a8770bc9a20851472f3059c116e
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-21 17:13:02 +0200

    :truck: Move image display into proper Component

--- app/photo/[tag]/page.tsx
+++ app/photo/[tag]/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Header from "@/app/Components/Header/page";
 import styles from "../../page.module.css";
 import handleFetch from "@/utils/fetch";
+import PhotoDisplay from "./PhotoDisplay";
 
 const Tag = async ({ params }: any) => {
   const tag = params.tag;
-
   const data = await handleFetch.fetchAllImages(tag);
 
   return (
@@ -13,23 +12,7 @@ const Tag = async ({ params }: any) => {
       <main className={styles.main}>
         <Header />
         <div className={styles.main_wrapper}>
-          <h3 className={styles.page_title}>
-            <br />
-            {data.folderName}
-          </h3>
-          <div className={styles.images_container}>
-            {data.imagesArray.map((item: any) => (
-              <div key={item.asset_id} className={styles.image_wrapper}>
-                <Image
-                  src={item.url}
-                  fill
-                  alt="Picture of the author"
-                  className={styles.image}
-                  sizes="(max-width: 1440px) 100vw, 50vw"
-                ></Image>
-              </div>
-            ))}
-          </div>
+          <PhotoDisplay data={data} />
         </div>
       </main>
     </>
