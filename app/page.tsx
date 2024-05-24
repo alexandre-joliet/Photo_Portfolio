d2895857052e219c9bdc33a7b4d60a001af7dfc6
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-24 14:56:53 +0200

    :fire: Clean useless code

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,12 @@
 import styles from "./page.module.css";
 import Header from "./Components/Header/Header";
-import handleFetch from "@/utils/fetch";
 import { NextPage } from "next";
-import Image from "next/image";
 
 const Home: NextPage = async () => {
-  // const data = await handleFetch.fetchAllImages();
-
-  // console.log(data);
-
   return (
     <>
       <main className={styles.main}>
         <Header />
-        {/* <PhotoDisplay /> */}
-
-        {/* MAP */}
-        {/* {data.map((item: any) => (
-          <div key={item.id} className={styles.image_wrapper}>
-            <Image
-              src={item.url}
-              fill
-              alt="Picture of the author"
-              className={styles.image}
-            ></Image>
-          </div>
-        ))} */}
       </main>
     </>
   );
