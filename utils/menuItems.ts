0eb853aeb06d6232f88d90be8f2443cbc5e76d66
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-27 16:15:29 +0200

    :pencil2: Add nav items array

---
+++ utils/menuItems.ts
@@ -0,0 +1,34 @@
+export const menuItems = {
+
+normandyBeachRace: {
+  title: "Normandy Beach Race",
+  content: [
+    {
+      title: "2021",
+      url: "/photo/normandy_beach_race21"
+    },
+    {
+      title: "2023",
+      url: "/photo/normandy_beach_race23"
+    }
+  ],
+},
+
+usMotorShow: {
+  title: "US Motor Show",
+  content: [
+    {
+      title: "2022 - Classic",
+      url: "/photo/us_motor_show22-classic"
+    },
+    {
+      title: "2022 - Modern",
+      url: "/photo/us_motor_show22-modern"
+    }
+  ],
+},
+
+
+};
+
+// export default menuItems;
