c4f9aaf8c02f9b416b81cd99c870d82392224712
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-27 14:32:38 +0200

    :truck: Rename component

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import styles from "./page.module.css";
-import Nav from "../Nav/page";
+import Nav from "../Nav/Nav";
 
 const Header = () => {
   return (
