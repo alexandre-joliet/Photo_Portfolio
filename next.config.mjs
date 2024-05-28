e7bb1a03a6031b3d77748a31232dcf1cbd01b55b
alexandre-joliet <alexandre.joliet@gmail.com>
2024-05-28 14:27:53 +0200

    :boom: Remove image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const account = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
