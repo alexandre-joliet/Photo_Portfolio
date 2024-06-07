e58f19d35e8f8f2a39791a808c2efd1491a86272
alexandre-joliet <alexandre.joliet@gmail.com>
2024-06-07 15:41:50 +0200

    :art: Add link to Home to header

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,12 +1,15 @@
 "use client";
 import styles from "./header.module.css";
 import Nav from "../Nav/Nav";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header_title_wrapper}>
-        <h1 className={styles.header_title}>Alexandre Joliet</h1>
+        <Link href="/">
+          <h1 className={styles.header_title}>Alexandre Joliet</h1>
+        </Link>
         <h2 className={styles.header_subtitle}>Photographies</h2>
       </div>
       <Nav />
