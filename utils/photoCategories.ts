857a1868ba0974ae1b87fb7f44c1175c27d5ff03
alexandre-joliet <alexandre.joliet@gmail.com>
2024-07-04 16:28:18 +0200

    :pencil2: Add photo categories array

---
+++ utils/photoCategories.ts
@@ -0,0 +1,25 @@
+export const photoCategories = [
+  {
+    url: "/photo/animaux",
+    title: "Animaux",
+    picture: { src: "/images/animaux.avif", width: 100, height: 100 },
+  },
+
+{
+    url: "/photo/portraits",
+    title: "Portraits",
+    picture: { src: "/images/portrait.avif", width: 100, height: 100 },
+  },
+  
+  {
+    url: "/photo/vehicules",
+    title: "Véhicules",
+    picture: { src: "/images/vehicule.avif", width: 100, height: 100 },
+  },
+
+ {
+    url: "/photo/voyage",
+    title: "Voyage",
+    picture: { src: "/images/voyage.avif", width: 100, height: 100 },
+  },
+]
