ed1f45041ed66e46207f9d591fc8877213b775bf
alexandre-joliet <alexandre.joliet@gmail.com>
2024-07-07 01:10:20 +0200

    :art: Update urls

--- utils/menuItems.ts
+++ utils/menuItems.ts
@@ -1,44 +1,40 @@
 export const menuItems = {
 
-normandyBeachRace: {
-  title: "Normandy Beach Race",
-  content: [
-    {
-      title: "2023",
-      url: "/photo/normandy_beach_race23"
-    },
-    {
-      title: "2021",
-      url: "/photo/normandy_beach_race21"
-    },
-  ],
-},
+  normandyBeachRace: {
+    title: "Normandy Beach Race",
+    content: [
+      {
+        title: "2023",
+        url: "/photo/Vehicules/normandy_beach_race23"
+      },
+      {
+        title: "2021",
+        url: "/photo/Vehicules/normandy_beach_race21"
+      },
+    ],
+  },
 
-leMans24h: {
-  title: "24h Le Mans",
-  content: [
-    {
-      title: "2024",
-      url: "/photo/24h_le_mans24"
-    },
-  ],
-},
+  leMans24h: {
+    title: "24h Le Mans",
+    content: [
+      {
+        title: "2024",
+        url: "/photo/Vehicules/24h_le_mans24"
+      },
+    ],
+  },
 
-usMotorShow: {
-  title: "US Motor Show",
-  content: [
-    {
-      title: "2022 - Classic",
-      url: "/photo/us_motor_show22-classic"
-    },
-    {
-      title: "2022 - Modern",
-      url: "/photo/us_motor_show22-modern"
-    }
-  ],
-},
-
-
+  usMotorShow: {
+    title: "US Motor Show",
+    content: [
+      {
+        title: "2022 - Classic",
+        url: "/photo/Vehicules/us_motor_show22-classic"
+      },
+      {
+        title: "2022 - Modern",
+        url: "/photo/Vehicules/us_motor_show22-modern"
+      }
+    ],
+  },
 };
-
-// export default menuItems;
