0262626ef6a43144386189aa132e71f0f84b4a49
alexandre-joliet <alexandre.joliet@gmail.com>
2024-07-07 01:07:11 +0200

    :sparkles: Add info for photo folders

---
+++ utils/foldersDetails.ts
@@ -0,0 +1,123 @@
+export const folderDetails = [
+
+  // ***** ANIMAUX *****
+ {
+    title: "Animaux",
+    text: "",
+    tag: "",
+  },
+
+ {
+    title: "La Vallée des Singes - 2024",
+    text: "",
+    tag: "vallee-des-singes_2024",
+  },
+
+  
+  // ***** PORTRAITS *****
+ {
+    title: "Portraits",
+    text: "",
+    tag: "",
+  },
+  {
+    title: "Mariana - Sunflowers",
+    text: "",
+    tag: "portraits_mariana-sunflowers",
+  },
+ 
+  // ***** VEHICULES *****
+ {
+    title: "Véhicules",
+    text: "",
+    tag: "",
+  },
+  {
+    title: "24h Le Mans 2024",
+    text: "",
+    tag: "24h_le_mans24",
+  },
+  {
+    title: "Normandy Beach Race 2023",
+    text: "",
+    tag: "normandy_beach_race23",
+  },
+  {
+    title: "Normandy Beach Race 2021",
+    text: "",
+    tag: "normandy_beach_race21",
+  },
+
+  {
+    title: "US Motor Show 2022 - Classic",
+    text: "",
+    tag: "us_motor_show22-classic",
+  },
+  {
+    title: "US Motor Show 2022 - Modern",
+    text: "",
+    tag: "us_motor_show22-modern",
+  },
+
+  // ***** VOYAGE *****
+  {
+    title: "Voyage",
+    text: "",
+    tag: "",
+  },
+  {
+    title: "2023.04 - Menorca",
+    text: "",
+    tag: "202304_menorca",
+  },
+  {
+    title: "2022.07 - Bretagne",
+    text: "",
+    tag: "202207_bretagne",
+  },
+  {
+    title: "2021.07 - Corse",
+    text: "",
+    tag: "202106_corse",
+  },
+  {
+    title: "2020.03 - Sri Lanka",
+    text: "",
+    tag: "202003_srilanka",
+  },
+  {
+    title: "2019.07 - Lozère et Vercors",
+    text: "",
+    tag: "201907_lozere",
+  },
+  {
+    title: "2018.10 - Bali",
+    text: "",
+    tag: "201810_bali",
+  },
+  {
+    title: "2018.05 - Venise",
+    text: "",
+    tag: "201805_venise",
+  },
+  {
+    title: "2017.10 - Bali",
+    text: "",
+    tag: "201710_bali",
+  },
+  {
+    title: "2017.01 - Dubaï Marina by Night",
+    text: "",
+    tag: "201701_dubaimarina",
+  },
+  {
+    title: "2017.01 - Dubaï",
+    text: "",
+    tag: "201701_dubai",
+  },
+  {
+    title: "2017.01 - Abu Dhabi",
+    text: "",
+    tag: "201701_abudhabi",
+  },
+];
