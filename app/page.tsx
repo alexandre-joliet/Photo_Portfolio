143d5a8f89ecd5a4cbca5939e346e7748510e138
alexandre-joliet <alexandre.joliet@gmail.com>
2024-07-12 17:59:44 +0200

    :truck: Move bio into proper page

--- app/page.tsx
+++ app/page.tsx
@@ -12,30 +12,10 @@ const Home: NextPage = async () => {
       <main className={styles.main}>
         <Header />
         <div className={styles.main_wrapper}>
-          <article className={styles.intro_content}>
-            <p className={styles.intro_title}>Bienvenue !</p>
-            <p className={styles.intro_text}>
-              Je m&apos;appelle Alexandre Joliet et je suis photographe amateur
-              passionné. Vous trouverez ici une sélection de photographies
-              réalisées par mes soins.
-            </p>
-            <p className={styles.intro_text}>
-              Découvrez les différentes catégories ci-dessous et accédez à tous
-              les albums depuis le menu à tout moment.
-            </p>
-            <p className={styles.intro_text}>
-              J&apos;espère que vous prendrez autant de plaisir à explorer mes
-              œuvres que j&apos;en ai eu à les créer.
-            </p>
-            <p>Bon visionnage !</p>
-          </article>
           <div className={styles.images_container}>
             {data.folders.map((item: any) => (
               <CategoryCard data={item} key={item.name}></CategoryCard>
             ))}
-
-            {/* <CategoryCard data={testCategory}></CategoryCard>
-            <CategoryCard data={testCategory}></CategoryCard> */}
           </div>
         </div>
       </main>
