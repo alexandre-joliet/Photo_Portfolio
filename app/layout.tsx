59d7031becc715e696bfd7d720b99bd7ebd0ab58
alexandre-joliet <alexandre.joliet@gmail.com>
2024-09-09 14:30:11 +0200

    :construction: Test preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 import type { Metadata } from "next";
+import ogImage from "../public/images/opengraph-image.jpg";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Alexandre Joliet - Photographies",
   description:
     "Photographe amateur passionné, je présente ici une sélection de photographies réalisées par mes soins. Bon visionnage !",
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
